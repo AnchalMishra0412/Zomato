5219e159b2298f8b03ae61ab4bccc100f9671ca1
AnchalMishra0412 <04anchalmishra@gmail.com>
2025-11-21 23:53:02 +0530

    Create main.tf

---
+++ terraformfiles/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "test-server" {
+  ami = "ami-0d176f79571d18a8f"
+  instance_type = "t3.small"
+  key_name = "MyNewKeyPair"
+  vpc_security_group_ids = ["sg-0126d9f5bc3c4c895"]
+  connection {
+     type = "ssh"
+     user = "ec2-user"
+     private_key = file("./MyNewKeyPair.pem")
+     host = self.public_ip
+     }
+  provisioner "remote-exec" {
+     inline = ["echo 'wait to start the instance' "]
+  }
+  tags = {
+     Name = "test-server"
+     }
+  provisioner "local-exec" {
+     command = "echo ${aws_instance.test-server.public_ip} > inventory"
+     }
+  provisioner "local-exec" {
+     command = "ansible-playbook /var/lib/jenkins/workspace/zomatoapp/terraform-files/ansibleplay.yml"
+     }
+  }
